bedcc31154939df44322476057f604f197027200
sewook11 <makato08@naver.com>
2025-06-12 10:14:26 +0000

    Deploy from GitHub Actions

--- src/app/mypage/_components/LeaderMeetingCardList.tsx
+++ src/app/mypage/_components/LeaderMeetingCardList.tsx
@@ -3,6 +3,7 @@ import LeaderMeetingCard, { LeaderMeetingCardProps } from "./LeaderMeetingCard";
 import { useState } from "react";
 import Button from "@/components/common/Button";
 import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 type LeaderMeetingListProps = {
   meetings: LeaderMeetingCardProps[];
@@ -19,11 +20,11 @@ const LeaderMeetingList = ({
   perPage = 10,
   showPagination = false,
 }: LeaderMeetingListProps) => {
+  const router = useRouter();
   const totalPages = Math.ceil(meetings.length / perPage);
   const paginated = showPagination
     ? meetings.slice((currentPage - 1) * perPage, currentPage * perPage)
     : meetings.slice(0, 3); // 요약용 기본 3개
-
 
   const [status, setStatus] = useState("");
 
@@ -41,12 +42,23 @@ const LeaderMeetingList = ({
             <option value="모집중">모집중</option>
             <option value="완료">완료</option>
           </select>
-          <button onClick={() => console.log("조회 클릭")} className="w-16 h-10 text-xs p-0 rounded-md bg-primary">조회</button>
+          <button
+            onClick={() => console.log("조회 클릭")}
+            className="w-16 h-10 text-xs p-0 rounded-md bg-primary"
+          >
+            조회
+          </button>
         </div>
-        <Button onClick={() => console.log("모임 생성하기 클릭")} className="text-white flex text-xs gap-1 font-semibold" color="accent" width="w-auto" height="h-10">
+        <Button
+          onClick={() => router.push("/meet/create")}
+          className="text-white flex text-xs gap-1 font-semibold"
+          color="accent"
+          width="w-auto"
+          height="h-10"
+        >
           <Plus size={20} strokeWidth={3} />
           모임 생성하기
-          </Button>
+        </Button>
       </div>
       <div className="space-y-4">
         {paginated.map((meeting, idx) => (
